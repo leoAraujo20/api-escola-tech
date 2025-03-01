ff777b00c47f3a0b8ff2890640156970860a8012
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-03-01 09:29:15 -0300

    initialize Django project with basic structure and configurations

---
+++ escola/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from escola.models import Estudante, Curso
+
+# Register your models here.
+class EstudanteAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'email', 'cpf', 'data_nascimento', 'celular',)
+    list_display_links = ('id', 'nome',)
+    search_fields = ('nome',)
+    list_per_page = 20
+
+admin.site.register(Estudante, EstudanteAdmin)
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'codigo', 'descricao')
+    list_display_links = ('id', 'codigo',)
+    search_fields = ('codigo',)
+
+admin.site.register(Curso, CursoAdmin)
