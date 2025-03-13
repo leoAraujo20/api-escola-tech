e66885cf22c13c2581eced0d7fa2be312a15717d
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-03-13 11:40:24 -0300

    Add tests for Matriculas

--- escola/tests/test_matricula.py
+++ escola/tests/test_matricula.py
@@ -2,9 +2,53 @@ from rest_framework.test import APITestCase, force_authenticate
 from django.urls import reverse
 from rest_framework import status
 from django.contrib.auth.models import User
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import MatriculaSerializer
 
 class MatriculasTestCase(APITestCase):
     def setUp(self):
         self.usuario = User.objects.create_superuser(username='admin',password='admin')
         self.url = reverse('Matriculas-list')
         self.client.force_authenticate(user=self.usuario)
+        self.estudante = Estudante.objects.create(
+            nome='Estudante Teste',
+            email='estudante@gmail.com',
+            cpf='77567543010',
+            data_nascimento='2003-02-02',
+            celular='11 98765-4321'
+        )
+        self.curso = Curso.objects.create(
+            codigo='CTT',descricao='Curso Teste',nivel='B'
+        )
+        self.matricula = Matricula.objects.create(
+            estudante=self.estudante,
+            curso=self.curso,
+            periodo='M'
+        )
+    
+    def test_listar_matriculas(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK) 
+
+    def test_criar_matricula(self):
+        dados = {
+            'estudante': self.estudante.pk,
+            'curso': self.curso.pk,
+            'periodo': 'M'
+        }
+        response = self.client.post(self.url, data=dados)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_atualizar_matricula(self):
+        dados = {
+            'estudante': self.estudante.pk,
+            'curso': self.curso.pk,
+            'periodo': 'V'
+        }
+        response = self.client.put(f'{self.url}1/', data=dados)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    def test_remover_matricula(self):
+        response = self.client.delete(f'{self.url}1/')
+        self.assertEqual(response.status_code,status.HTTP_405_METHOD_NOT_ALLOWED)
+    
